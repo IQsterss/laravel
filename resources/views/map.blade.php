7612903eac3b5de1bc51cf5e70126e0888e54456
IQsterss <stud126791@vyatsu.ru>
2023-06-28 19:59:24 +0300

    marks_on_map

--- resources/views/map.blade.php
+++ resources/views/map.blade.php
@@ -2,17 +2,34 @@
 <head>
         <script src="https://api-maps.yandex.ru/2.1/?apikey=b1de3bee-5003-4e47-bf6e-e6ea27bd2ef0&lang=ru_RU"></script>
         <script type="text/javascript">
+        var map; // Объявляем переменную для карты
+        var placemark; // Объявляем переменную для метки   
+        var userPlacemark;
+        var placemarks = [];
+        
         // Функция для инициализации карты
         ymaps.ready( function () {
         function init(latitude, longitude){
-            var map = new ymaps.Map("map", {
-                center: [latitude, longitude],
-                zoom: 12 } , {
+            map = new ymaps.Map("map", {
+            center: [latitude, longitude],
+            zoom: 12 } , {
             searchControlProvider: 'yandex#search'
             });
             // Создание метки на карте
-            var placemark = new ymaps.Placemark([latitude, longitude]);
+            userPlacemark = new ymaps.Placemark([latitude, longitude]);
+            map.geoObjects.add(userPlacemark);
+        }
+
+        function placeMarker(latitude, longitude) {
+            placemark = new ymaps.Placemark([latitude, longitude]);
             map.geoObjects.add(placemark);
+            placemarks.push(placemark);
+            map.panTo([latitude, longitude], { flying: true });
+        }
+
+        function removeMarkers() {
+            map.geoObjects.removeAll();
+            placemarks = [];
         }
 
         //получение геолокации и местонахождения пользователя
@@ -34,37 +51,37 @@
                 console.log("Геолокация не поддерживается в вашем браузере");
             }
         }
-        getUserLocation();
-        });
-        </script>
-        
-        <!-- function setMarker() {
-        var latitude = parseFloat(document.getElementById('latitudeInput').value);
-        var longitude = parseFloat(document.getElementById('longitudeInput').value);
 
-        if (isNaN(latitude) || isNaN(longitude)) {
-            alert('Invalid coordinates');
-            return;
+        function handlePlaceMarker() {
+            var latitudeInput = document.getElementById("latitude");
+            var longitudeInput = document.getElementById("longitude");
+
+            var latitude = parseFloat(latitudeInput.value);
+            var longitude = parseFloat(longitudeInput.value);
+
+            if (!isNaN(latitude) && !isNaN(longitude)) {
+                placeMarker(latitude, longitude);
+            } else {
+                console.log("Неверные координаты");
+            }
         }
 
-        var coords = [latitude, longitude];
+        function handleRemoveMarkers() {
+            removeMarkers();
+        }
 
-        // Create a placemark at the entered coordinates
-        var myPlacemark = new ymaps.Placemark(coords, {}, {
-            preset: 'islands#redDotIcon'
+        getUserLocation();
+        
+        
+        var placeMarkerButton = document.getElementById("place-marker-button");
+        placeMarkerButton.addEventListener("click", handlePlaceMarker);
+
+        var removeMarkersButton = document.getElementById("remove-markers-button");
+        removeMarkersButton.addEventListener("click", handleRemoveMarkers);
         });
-
-        // Add the placemark to the map
-        myMap.geoObjects.add(myPlacemark);
-
-        // Center the map on the marker
-        myMap.setCenter(coords);
-        myMap.setZoom(13);
-    } -->
-    
-    
-        </head>
-    <x-app-layout>
+    </script>
+</head>
+<x-app-layout>
         <x-slot name="header">
             <h2 class="font-semibold text-xl text-gray-800 leading-tight">
                 {{ __('Map') }}
@@ -75,21 +92,12 @@
             <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900">
-                <!-- <div>
-                        <x-input-label for="latitudeInput" :value="__('Широта: ')" />
-                        <x-text-input id="latitudeInput" name="latitudeInput" type="text" required  />  <!--class="mt-1 block w-full" :value="old('latitudeInput', $user->latitudeInput)"
-                        <!- <x-input-error class="mt-2" :messages="$errors->get('name')" />  -->
-
-                        <!-- <x-input-label for="longitudeInput" :value="__('Долгота: ')" /> -->
-                        <!-- <x-text-input id="longitudeInput" name="longitudeInput" type="text" required  /> class="mt-1 block w-full" :value="old('longitudeInput', $user->longitudeInput)" required   /> -->
-                        <!-- <x-input-error class="mt-2" :messages="$errors->get('name')" /> -->
-
-                        <!-- <x-primary-button>{{ __('Поставить метку') }}</x-primary-button> -->
-                    <!-- </div> --> 
-                <div>
-                    Широта: <input type="text" id="latitudeInput">
-                    Долгота: <input type="text" id="longitudeInput">
-                    <x-primary-button>{{ __('Поставить метку') }}</x-primary-button>
+                    <label for="latitude">Широта:</label>
+                    <input type="text" id="latitude">
+                    <label for="longitude">Долгота:</label>
+                    <input type="text" id="longitude">
+                    <x-primary-button id="place-marker-button" >Поставить метку</x-primary-button>
+                    <x-primary-button id="remove-markers-button">Удалить все метки</x-primary-button>
                 </div>
                     <div id="map" style="width: 600px; height: 400px;"></div>
                 </div>
